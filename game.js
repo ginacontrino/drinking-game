6bf71641c37eaf513e5b3f2297435d8de7ee6d33
Gina Contrino <g.contrino@hotmail.de>
2015-01-09 01:21:06 +0100

    fix formatting

--- game.js
+++ game.js
@@ -5,42 +5,17 @@ playGame();
 // Functions
 // ======================================
 
-/**
- * Tells you if the number you entered is equal
- * to the secret number
- *
- * @param integer guessNumber
- * @param integer secretNumber
- * @return string
- */
-function compareGuessNumberToSecretNumber(guessNumber, secretNumber)
-{
-  // if guessed number too high or too low return have a drink
-  if (guessNumber>getMaxNumber() || guessNumber < 1)
-  {
-    return "Penalty! Finish your drink! Your number must be between 1 and " + getMaxNumber() + ".";
-  }
-  // if the guessed number is wrong return secret number
-  if (guessNumber<secretNumber || guessNumber>secretNumber)
-  {
-    return guessNumber + ". No drink for you. The secret Number is " + secretNumber + ".";
-  }
-
-  // otherwise the guessed number equals the secret number return winner
-  return guessNumber + " Oh no! Have a drink!";
-
-}
 
 /**
  * Plays the game
  *
  * @return void
  */
-
 function playGame()
 {
   console.log( compareGuessNumberToSecretNumber(getTheFirstArgument(), getSecretNumber()));
 }
+
 
 /**
  * Gets the secret number for the game
@@ -49,29 +24,20 @@ function playGame()
  */
 function getSecretNumber()
 {
-  return generateRandomNumber1to4();
+  return generateRandomNumber1toX();
 }
 
 
 /**
- * Returns the first commmand line argument
- *
- * @return string
- */
-function getTheFirstArgument()
-{
-  return process.argv[2];
-}
-
-/**
- * Gets a random secret number between 1 and 5 for the game
+ * Gets a random secret number between 1 and x for the game
  *
  * @return integer
  */
-function generateRandomNumber1to4 ()
+function generateRandomNumber1toX()
 {
   return Math.round(Math.random() * (getMaxNumber()-1)) + 1;
 }
+
 
 /**
  * Sets max. number in the game
@@ -81,4 +47,43 @@ function generateRandomNumber1to4 ()
 function getMaxNumber()
 {
   return 3;
+}
+
+
+/**
+* Tells you if the number you entered is equal
+* to the secret number
+*
+* @param integer guessNumber
+* @param integer secretNumber
+* @return string
+*/
+function compareGuessNumberToSecretNumber(guessNumber, secretNumber)
+{
+  // if guessed number too high or too low return have a drink
+  if (guessNumber>getMaxNumber() || guessNumber < 1)
+  {
+    return "Penalty! Finish your drink! Your number must be between 1 and " + getMaxNumber() + ".";
+  }
+
+  // if the guessed number is wrong return secret number
+  if (guessNumber<secretNumber || guessNumber>secretNumber)
+  {
+    return "Number " + guessNumber + ". No drink for you. The secret number is " + secretNumber + ".";
+  }
+
+  // otherwise the guessed number equals the secret number return winner
+  return "Oh no! Number " + guessNumber + ". Have a drink!";
+
+}
+
+
+/**
+* Returns the first commmand line argument
+*
+* @return string
+*/
+function getTheFirstArgument()
+{
+  return process.argv[2];
 }
